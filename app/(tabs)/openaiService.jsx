a890a080e2c303f4c935e05557bb4a98c3b1f0df
Nakshatra0707 <nakshatra.dubey@gmail.com>
2025-01-11 19:12:34 +0530

    commit

--- app/(tabs)/openaiService.jsx
+++ app/(tabs)/openaiService.jsx
@@ -1,7 +1,7 @@
 // openaiService.js
 import axios from 'axios';
 
-const apiKey = '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';
+const apiKey = '';
 
 export const createCompletion = async (messages) => {
   try {
